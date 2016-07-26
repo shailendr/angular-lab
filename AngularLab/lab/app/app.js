08f3570c0fa4fec494af1057a246122de0ef6c94
Dev <dev@localhost.localdomain>
2016-07-26 17:01:19 -0400

    added task and member view

--- AngularLab/lab/app/app.js
+++ AngularLab/lab/app/app.js
@@ -1,4 +1,20 @@
-var app = angular.module("teamApp", []);
+var app = angular.module("teamApp", ["ngRoute"]);
+
+app.config(function($routeProvider){
+	$routeProvider
+	.when("/",{
+		templateUrl : "directives/login.html",
+		controller : "loginCtrl"
+	})
+	.when("/login",{
+		templateUrl : "directives/login.html",
+		controller : "loginCtrl"
+	})
+	.when("/team",{
+		templateUrl : "views/team/members.html",
+		controller : "teamCtrl"
+	});
+});
 
 app.directive("header", function() {
     return {
@@ -30,24 +46,37 @@ app.directive("task", function() {
 
 app.controller('teamAppCtrl',function($scope, $rootScope){
     $rootScope.isLogin = false;
-    $rootScope.users = [{email:'admin@admin.com', password:'admin123' }];
+    $rootScope.view= "login";
     $rootScope.activeTeam = "";
 });
 
+var members = [
+				{
+				 "firstName" : "Shailendra",
+				 "lastName"  : "Singh",
+				 "address"   : "Pune, Maharastra",
+				 "avatar"	 : "../../assets/images/male.png",
+				 "task"		 : "2"	
+				}
+			];
 
+app.controller('teamCtrl',function($scope, $rootScope){
+    $scope.members = members; 
+});
 
-app.controller('loginCtrl',function($scope, $rootScope){
+var users = [{email:'admin@admin.com', password:'admin123' }];
+
+app.controller('loginCtrl',function($scope, $rootScope, $location){
 	$scope.email="";
 	$scope.password="";
     $scope.subLogin = function(){
-    	var users = $rootScope.users;
     	angular.forEach(users, function(value, key) {
 		  if(value.email === $scope.email && value.password === $scope.password){
 		  	$rootScope.isLogin = true;
 		  	$rootScope.activeTeam = "active";
+			$location.path('/team');
 		  }
 		});
-    	// alert($scope.email + " " + $scope.password);
     };
     
 });
